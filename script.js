de621b0ad7220770ba43a4c9648e799f2da2a2f8
trinidj <jm.trinidad199@gmail.com>
2025-07-04 21:53:43 -0400

    update

--- script.js
+++ script.js
@@ -7,42 +7,62 @@ class Calculator {
   }
 
   handleNumberClick = value => {
-    this.currentValue = value;  
+    if (this.currentValue === '0') {
+      this.currentValue = value;
+    } else {
+      this.currentValue += value;
+    }
     this.updateDisplay();
   }
 
   updateDisplay = () => {
-    this.display.value += this.currentValue;
+    this.display.value = this.currentValue;
   }
 
   clearScreen = () => {
-    this.display.value = '';
+    this.currentValue = '0';
+    this.previousValue = null;
+    this.updateDisplay();
   }
 
   calculate = (num1, op, num2) => {
-    if (this.currentValue.length < 0) {
-      alert('please choose a number first');
-    } else {
-      
+    let result;
+    const a = parseFloat(num1);
+    const b = parseFloat(num2);
+
+    switch (op) {
+      case '+':
+        result = a + b;
+        break;
+      default:
+        result = 'Error';
     }
+    this.currentValue = result.toString();
+    this.updateDisplay();
   }
 }
 
-const calculator = new Calculator();
-
+const calc = new Calculator();
 const display = document.getElementById('display');
 const numberButtons = document.getElementsByClassName('number-btn');
 const clearButton = document.getElementById('clear-btn');
 const decimalButton = document.getElementById('decimal-btn');
 
+const addButton = document.getElementById('add-btn');
+const subtractButton = document.getElementById('subtract-btn');
+const multiplyButton = document.getElementById('multiply-btn');
+const divideButton = document.getElementById('divide-btn');
+const percentButton = document.getElementById('percent-btn');
+const equalsButton = document.getElementById('equals-btn');
+
 Array.from(numberButtons).forEach(button => {
   button.addEventListener('click', (e) => {
-    calculator.handleNumberClick(e.target.value);
+    calc.handleNumberClick(e.target.value);
   });
 });
 
 clearButton.addEventListener('click', () => {
-  display.value = '';
+  calc.clearScreen();
 });  
 
 decimalButton.addEventListener('click', () => {
@@ -52,3 +72,13 @@ decimalButton.addEventListener('click', () => {
     display.value += '.';
   }
 });
+
+addButton.addEventListener('click', () => {
+  calc.operator = '+';
+  calc.previousValue = calc.currentValue;
+  calc.currentValue = '';
+})
+
+equalsButton.addEventListener('click', () => {
+  calc.calculate(calc.previousValue, calc.operator, calc.currentValue);
+})
